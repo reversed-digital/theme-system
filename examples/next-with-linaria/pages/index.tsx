fdeefcc671b22d71aca0064d7aba1eb4b3716f19
Corjen Moll <corjen@thinkreversed.com>
2020-08-07 09:18:07 +0200

    Add example

---
+++ examples/next-with-linaria/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+import { filterProps } from 'theme-system'
+
+export default function Home() {
+  console.log(filterProps)
+  return <div>Example</div>
+}
